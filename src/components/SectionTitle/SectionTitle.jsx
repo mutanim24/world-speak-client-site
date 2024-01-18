3a64eaf84d452daa445bc14e205965f49e24b100
mutanim24 <mutanim24@gmail.com>
2024-01-18 23:43:59 +0600

    added google font

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SectionTitle = ({title, subtitle, textColor}) => {
     return (
-        <div className={`text-center my-5 p-3 mx-auto ${textColor}`}>
+        <div className={`text-center my-5 p-3 mx-auto font-lemon ${textColor}`}>
            <h1 className=' text-2xl md:text-3xl font-semibold mb-3 uppercase'>{title}</h1> 
            <p>{subtitle}</p>
         </div>
